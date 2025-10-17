0a6e6cf6ceb73b10b1f0540f1e21b1b951aebc68
Travis Born <tborn9@gmail.com>
2025-10-16 23:58:53 -0600

    Cleaned up old root-level Javadoc files

--- app/javadocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.listycity","l":"City"},{"p":"com.example.listycity","l":"CityList"}];updateSearchResults();
